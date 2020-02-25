1ec48b1d59e9b3a6b599e6478e618d6a028dbfa3
lilei <381128431@qq.com>
2020-02-25 16:46:08 +0800

    添加解析bit数据文件的程序，还未进行测试

--- DataHandle.c
+++ DataHandle.c
@@ -16,13 +16,13 @@
 
 
 /**
- * @fn SaveCoilBits
- * @brief 保存离散线圈的数据
+ * @fn SaveBitsFile
+ * @brief 保存离散线圈的数据到文件
  * @param tabBits 需要保存的数据
  * @param modbusCoil 数据的相关信息结构体
  * @return 成功:0 错误:-1
  */
-int SaveBits(uint8_t *tabBits, ConfigFile *modbusCoil)
+int SaveBitsFile(uint8_t *tabBits, ConfigFile *modbusCoil)
 {
     int fileFd = 0;         //文件描述符
     ssize_t writeNum = 0;   //写入的数据长度
@@ -65,13 +65,13 @@ int SaveBits(uint8_t *tabBits, ConfigFile *modbusCoil)
 
 
 /**
- * @fn SaveRegisters
- * @brief 保存寄存器的数据
+ * @fn SaveRegistersFile
+ * @brief 保存寄存器的数据到文件
  * @param tabRegisters 需要保存的数据
  * @param modbusRegister 数据的相关信息结构体
  * @return 成功:0 错误:-1
  */
-int SaveRegisters(uint16_t *tabRegisters, ConfigFile *modbusRegister)
+int SaveRegistersFile(uint16_t *tabRegisters, ConfigFile *modbusRegister)
 {
     int fileFd = 0;         //文件描述符
     ssize_t writeNum = 0;   //写入的数据长度
@@ -123,16 +123,16 @@ int SaveDataInfo(int filefd, ConfigFile *dataInfo)
 
 
 /**
- * @fn Int2Char
+ * @fn Int2String
  * @brief 整型数转字符串
  * @param number 需要转换的数
  * @param string 转换后的字符串
  * @return 成功:转换后字符串长度 错误:-1
  */
-int Int2Char(int number, char *string)
+int Int2String(int number, char *string)
 {
     int i = 0;
-    bool isPositive = true;
+    bool isPositive = true;     //正数标志位
     int temp = number;
 
     if(string == NULL)
@@ -169,7 +169,7 @@ int SaveIntToFile(int fileFd, int number)
     char string[20] = {0};  //存放数字转换后的字符串
     int stringLenth = 0;
 
-    stringLenth = Int2Char(number, string);
+    stringLenth = Int2String(number, string);
     if(stringLenth == -1)
         return -1;
     return SaveStringToFile(fileFd, string);
@@ -216,3 +216,158 @@ int GetTimeStr(char *timeStr)
     return 0;
 }
 
+
+/**
+ * @fn ParseBitsFile
+ * @brief 解析离散线圈的数据文件
+ * @param fileName 文件名
+ * @param tabBits 需要保存的数据
+ * @param tabNumber tabBits数组的大小
+ * @param modbusCoil 数据的相关信息结构体
+ * @return 成功:0 错误:-1
+ */
+int ParseBitsFile(char *fileName, uint8_t *tabBits, int tabNumber, ConfigFile *modbusCoil)
+{
+    int fileFd = 0;         //文件描述符
+    char modbusString[STRING_LENTH] = {0};    //字符串
+    int modbusNumber = 0;               //modbus的数量
+    int ret = -1, i = 0;
+    char tempChar = '0';
+
+    memset(tabBits, 0, tabNumber);
+
+    fileFd = open(fileName, O_RDONLY);      //打开需要解析的文件
+    if(fileFd < 0)
+		return -1;
+
+    /* 保存信息的格式：Data Name,数据名称,Start Address,起始地址,Number,数量, */
+    for( ; tempChar == ','; )
+    {
+        read(fileFd, &tempChar, 1);
+    }
+    ret = ReadStringFromFile(fileFd, modbusString, STRING_LENTH);
+    if(0 != ret)
+    {
+        printf("Read Modbus Name String Error!\n");
+        return -1;
+    }
+    ret = strncmp(modbusString, modbusCoil->dataName, STRING_LENTH);
+    if(0 != ret)
+    {
+        printf("Modbus Name Error!\n");
+        return -1;
+    }
+
+    for(i = 0; i < 3; )
+    {
+        read(fileFd, &tempChar, 1);
+        if(tempChar == ',')
+            i++;
+    }
+    ret = ReadStringFromFile(fileFd, modbusString, STRING_LENTH);
+    if(0 != ret)
+    {
+        printf("Read Modbus Number String Error!\n");
+        return -1;
+    }
+    modbusNumber = String2Int(modbusString, STRING_LENTH);
+    if(modbusNumber != modbusCoil->number)
+    {
+        printf("Modbus Number Error!\n");
+        return -1;
+    }
+
+    /* 解析数据 */
+    for( ; tempChar == '\n'; )
+    {
+        read(fileFd, &tempChar, 1);
+    }
+    for(i = 0; i < modbusCoil->number && i < tabNumber; i++)
+    {
+        ret = ReadStringFromFile(fileFd, modbusString, STRING_LENTH);
+        if(0 != ret)
+        {
+            printf("Read Modbus Data String Error!\n");
+            return -1;
+        }
+        modbusNumber = String2Int(modbusString, STRING_LENTH);
+        if(1 == modbusNumber || 0 == modbusNumber)
+        {
+            tabBits[i] = modbusNumber;
+        }
+
+        if((i%10) == 9)
+        {
+            read(fileFd, &tempChar, 1);
+        }
+    }
+    
+    return 0;
+}
+
+
+/**
+ * @fn ReadStringFromFile
+ * @brief 从文件中当前位置读出一个字符串，遇到','停止
+ * @param fileFd 文件描述符
+ * @param string 读出的字符串
+ * @param stringLenth 字符串数组长度
+ * @return 成功:0 错误:-1
+ */
+int ReadStringFromFile(int fileFd, char *string, int stringLenth)
+{
+    int stringNumber = 0;       //实际读到的字符串长度
+    char tempChar = 0;          //读出的单个字符
+    int i = 0;
+
+    memset(string, 0, stringLenth);     //清空一下string
+    for(i = 0; tempChar != ',' || tempChar != '\n'; )
+    {
+        stringNumber = read(fileFd, &tempChar, 1);
+        if(1 == stringNumber)
+        {
+            if(i < stringLenth)
+            {
+                string[i] = tempChar;
+                i++
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+    return 0;
+}
+
+
+/**
+ * @fn String2Int
+ * @brief 字符串转整型
+ * @param string 需要转换的字符串
+ * @param StringLenth 字符串长度
+ * @return 转换后的整数
+ */
+int String2Int(char *string, int StringLenth)
+{
+    int number = 0;         //保存转换后的数
+    bool isPositive = true;     //正数标志位
+    int i = 0;
+
+    for(i = 0; i < StringLenth || string[i] == '\0'; i++)
+    {
+        if(string[i] == '-')
+        {
+            isPositive = false;
+            continue;
+        }
+        number *= 10;
+        number += string[i] - '0';
+    }
+    if(isPositive == false)
+    {
+        number = -number;
+    }
+
+    return number;
+}
--- DataHandle.h
+++ DataHandle.h
@@ -12,13 +12,17 @@
 #ifndef _DATA_HANDLE_H_
 #define _DATA_HANDLE_H_
 
+#define STRING_LENTH DATA_NAME_LENTH_MAX
 
-int SaveBits(uint8_t *tabBits, ConfigFile *modbusCoil);
-int SaveRegisters(uint16_t *tabRegisters, ConfigFile *modbusRegister);
+int SaveBitsFile(uint8_t *tabBits, ConfigFile *modbusCoil);
+int SaveRegistersFile(uint16_t *tabRegisters, ConfigFile *modbusRegister);
 int SaveDataInfo(int filefd, ConfigFile *dataInfo);
-int Int2Char(int number, char *string);
+int Int2String(int number, char *string);
 int SaveIntToFile(int fileFd, int number);
 int SaveStringToFile(int fileFd, char *string, int stringLenth);
 int GetTimeStr(char *timeStr);
+int ParseBitsFile(char *fileName, uint8_t *tabBits, int tabNumber, ConfigFile *modbusCoil);
+int ReadStringFromFile(int fileFd, char *string, int stringLenth);
+int String2Int(char *string, int StringLenth);
 
 #endif
